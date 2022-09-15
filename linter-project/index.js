7559eec42d7fa4ebac930fa127d5c31af6973f8a
Victor Torres Valencia <victor.tv@wizeline.com>
2022-09-14 19:50:49 -0500

    Create linter project

---
+++ linter-project/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable semi */
+
+const nameA = 'holab17🙂';
+
+// string template
+const greeting  = () => console.log(`hola ${nameA}`)
+const goodBye  = () => console.log(`adios ${nameA}`)
+const goo  = () => console.log(`adios ${nameA}`)
+const goodBy  = () => console.log(`adios ${nameA}`)
+
+
+
+console.log(greeting());
+console.log(goodBye());
+console.log(goo());
+console.log(goodBy());
