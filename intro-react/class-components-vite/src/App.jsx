48c05ef67f2b877fc82fb0547cbaf20fa658ae68
Victor Torres Valencia <victor.tv@wizeline.com>
2022-09-20 19:11:43 -0500

    Parse to class component <App />

---
+++ intro-react/class-components-vite/src/App.jsx
@@ -0,0 +1,15 @@
+import { Component, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h3>React con clas components 👴🏽</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
