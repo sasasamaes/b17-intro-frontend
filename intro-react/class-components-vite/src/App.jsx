f3fee33a76ad91e537912ec4fcf20eb52d63d641
Victor Torres Valencia <victor.tv@wizeline.com>
2022-09-20 19:23:22 -0500

    Added basic JSX

--- intro-react/class-components-vite/src/App.jsx
+++ intro-react/class-components-vite/src/App.jsx
@@ -1,9 +1,11 @@
-import { Component, useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { Component } from "react";
 import "./App.css";
 
+// 1 extender de Component
 class App extends Component {
+  // poner el metodo de render()
   render() {
+    // JSX
     return (
       <div className="App">
         <h3>React con clas components 👴🏽</h3>
