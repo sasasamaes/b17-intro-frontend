5fb373f9d0b3afb22990aaccb3a09f1568d21f19
Victor Torres Valencia <victor.tv@wizeline.com>
2022-09-20 20:37:54 -0500

    Added condition for decrement in minimal value as 0

--- intro-react/class-components-vite/src/App.jsx
+++ intro-react/class-components-vite/src/App.jsx
@@ -1,15 +1,41 @@
-import { Component } from "react";
+import { Component, Fragment } from "react";
 import "./App.css";
 
 // 1 extender de Component
 class App extends Component {
-  // poner el metodo de render()
+  state = {
+    counter: 0,
+  };
+
+  handleClick = (type) => {
+    if (type === "inc") this.setState({ counter: this.state.counter + 1 });
+    else {
+      if (this.state.counter > 0) {
+        this.setState({
+          counter: this.state.counter - 1,
+        });
+      }
+    }
+  };
+
+  // 2 poner el metodo de render()
   render() {
     // JSX
     return (
-      <div className="App">
+      // React fragment
+      <>
         <h3>React con clas components 👴🏽</h3>
-      </div>
+        <div className="App">
+          <span>
+            Contador <b>{this.state.counter}</b>{" "}
+          </span>
+
+          <div className="btn-section">
+            <button onClick={() => this.handleClick("inc")}>Incrementar</button>
+            <button onClick={() => this.handleClick("dec")}>Decrementar</button>
+          </div>
+        </div>
+      </>
     );
   }
 }
