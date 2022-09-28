8bdfabae35b34c8870ebc0eb242f260b0897b2c9
Victor Torres Valencia <victor.tv@wizeline.com>
2022-09-27 19:21:42 -0500

    Added inputs and function to handle onChange

--- react-function/react-login/src/components/LoginForm.jsx
+++ react-function/react-login/src/components/LoginForm.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const LoginForm = () => {
-  return (
-    <div>LoginForm Component</div>
-  )
-}
+  const [credentials, setCredentials] = useState({});
 
-export default LoginForm
+  /**
+   * Tener dos inputs para el user y password
+   * & un boton para validar credenciales,
+   * Si las credenciales son correctas, dar acceso
+   *
+   * Harcodear 👀 credenciales con
+   * user: usuarioG17
+   * password: password
+   *
+   *
+   */
+
+  const handleFormValue = (e) => {
+    console.log(e.target.value);
+    // key: value
+    setCredentials();
+  };
+
+  return (
+    <div>
+      LoginForm Component
+      <div>
+        <input
+          type="text"
+          name="user"
+          value={credentials.user}
+          onChange={handleFormValue}
+        />
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={handleFormValue}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
