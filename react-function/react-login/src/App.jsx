7deeb7e57950b82d9135ea9f6d2293f09e4c1273
Victor Torres Valencia <victor.tv@wizeline.com>
2022-09-28 21:19:03 -0500

    Added Routes with path and JSX in elements

--- react-function/react-login/src/App.jsx
+++ react-function/react-login/src/App.jsx
@@ -1,20 +1,32 @@
 import { useState } from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
 import Dashboard from "./components/Dashboard";
 import LoginForm from "./components/LoginForm";
+import "./App.css";
 
 function App() {
   const [isUserLogged, setisUserLogged] = useState(false);
   return (
     <div className="App">
-      <h2>React app 📝</h2>
+      <Routes>
+        <Route path="" element={<h2>React app 📝</h2>} />
+        <Route path="/contact" element={<h2>Contactos de mi app 📓</h2>} />
+        <Route path="/shopping-cart" element={<h2>Carrito de compras 🛒</h2>} />
+        <Route path="/payment" element={<h2>Pago 💳</h2>} />
+
+        {/* DEFINIR RUTA 404 */}
+        <Route path="*" element={<h1> Aqui va página bonita de 404</h1>} />
+
+        {/* <h2>React app 📝</h2>
       <div className="card">
         {!isUserLogged ? (
           <LoginForm userLogged={(value) => setisUserLogged(value)} />
         ) : (
           <Dashboard />
         )}
-      </div>
+      </div> */}
+      </Routes>
     </div>
   );
 }
